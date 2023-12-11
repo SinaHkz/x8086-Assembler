e1bb2924c21945218a40aa868c95ebb329a0a0ac
SinaHkz <sina.hakimzadeh.1401@gmail.com>
2023-12-11 23:30:17 +0330

    add server and first successful run

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('/index.html')
+
+
+@app.route('/assembler', methods=['POST'])
+def run_script():
+    data = request.json
+    input_data = data['input']
+    # input_data = request.data.decode('utf-8')
+    with open('inputs.txt', 'w') as f:
+        f.write(input_data)
+
+    # Execute your Python script with the input
+    result = subprocess.check_output(['python', 'main.py', input_data], universal_newlines=True)
+    print(result)
+
+    return jsonify({'output': result})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
